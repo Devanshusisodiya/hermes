3f687f8e0fad705e1a1da58441e43af5e394eae5
Devanshusisodiya <devanshukumar45@gmail.com>
2024-02-19 14:50:18 +0530

    added methods to add agent to directory & directory access

--- hermes/storage.py
+++ hermes/storage.py
@@ -1,7 +1,43 @@
 import os
+import pickle as pkl
+from typing import Dict
+from hermes.agent import Agent
 
 class Storage:
     def __init__(self) -> None:
-        if "agent.json" not in os.listdir(os.getcwd()):
-            with open(os.getcwd() + "/agents.json", "w") as f:
-                pass
+        # uncomment to maintain a persistent agent storage
+        # if "agents.pkl" not in os.listdir(os.getcwd()):
+        self.agents_path = os.getcwd() + "/agents.pkl"
+        with open(self.agents_path, "wb") as f:
+            directory = {}
+            pkl.dump(directory, f)
+
+    async def update_directory(self, agent: Agent) -> bool:
+        """
+        function to update the agent directory
+        """
+        with open(self.agents_path, "rb") as f:
+            directory = pkl.load(f)
+
+        agent_exists = False # to keep track whether the agent exists
+    
+        # update the directory
+        if agent.name not in directory:
+            directory[agent.name] = agent
+            agent_exists = True
+        else:
+            print(f"Agent {agent.name} already exists on the network!!!")
+            # raise Exception("Agent already exists on the network!!!")
+            
+        with open(self.agents_path, "wb") as f:
+            pkl.dump(directory, f)
+
+        return agent_exists
+    
+    def access_directory(self) -> Dict:
+        """
+        function to access the agent directory
+        """
+        with open(self.agents_path, "rb") as f:
+            directory = pkl.load(f)
+        return directory
