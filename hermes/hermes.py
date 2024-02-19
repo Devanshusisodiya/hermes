aeef52e5a38d7fa7dd3352b57b55772bad5fbcc4
Devanshusisodiya <devanshukumar45@gmail.com>
2024-02-19 14:52:31 +0530

    initialising storage and agent layer

---
+++ hermes/hermes.py
@@ -0,0 +1,8 @@
+from hermes.storage import Storage
+from hermes.layer import Layer
+
+# initialising an agents storage 
+storage = Storage()
+
+# initialising a communication layer
+layer = Layer(storage=storage)
