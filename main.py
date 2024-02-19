cd1b73801771875454d96dfc46f4e9790341433d
Devanshusisodiya <devanshukumar45@gmail.com>
2024-02-19 14:53:25 +0530

    glue together

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-from hermes.layer import Layer
-from hermes.storage import Storage
+from hermes.agent import Agent
+from hermes.hermes import storage, layer
+
+a1 = Agent("a1")
+a2 = Agent("a2")
+
+layer.register(a1)
+layer.register(a2)
+
+# sending the message from agent a1 to agent a2
+a1.comm(layer, a2, "message from a1")
