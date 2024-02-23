6b1988240ab5d1f6715889e41395e3a82ae96a5e
Devanshusisodiya <devanshukumar45@gmail.com>
2024-02-23 20:54:28 +0530

    refactored multiplexer

--- hermes/multiplexer.py
+++ hermes/multiplexer.py
@@ -1,21 +1,19 @@
 import asyncio
-from asyncio import AbstractEventLoop
-import random
-from hermes.agent import Agent
+from hermes.agent_base import AgentBase
 
 class MultiPlexer:
     """
-    Transaction Layer
+    Multiplexer Layer
 
-    This class implements the Transaction layer in Hermes.
-    It contains methods to communicate to both the Storage Layer and the Agents.
+    This class implements the Multiplexer layer in Hermes.
+    It contains methods to communicate to both the Agents.
     """
 
     def __init__(self) -> None:
-        self.agents = {}
-        self.queue = asyncio.Queue()
+        self._agents = {}
+        self._queue = asyncio.Queue()
     
-    async def register(self, agent: Agent) -> bool:
+    async def register(self, agent: AgentBase) -> bool:
         """
         Function to register an agent
 
@@ -26,38 +24,49 @@ class MultiPlexer:
         """
         _registered = False
 
-        if agent.name in self.agents:
-            print("Agent already registered on the layer!!!")
+        if agent._hash in self._agents:
+            print(f"\nAgent already registered on the layer!!!\nHash: {agent._hash}\n")
         else:
-            self.agents[agent.name] = agent
+            self._agents[agent._hash] = agent
             _registered = True
 
         return _registered
 
-    async def schedule(self, agent_name: str, message: str) -> None:
+    async def schedule(self, agent_hash: str, message: str) -> None:
         """
         Sends the specified message to the recipent agent
 
             Parameters:
-                    recipient (str): Name of the agent
+                    agent_hash (str): Hash of the agent
                     message (str): Message to be sent
 
             Returns:
                     None
         """
-        await self.queue.put(
+        await self._queue.put(
             {
-                "receiver": agent_name,
+                "sender": agent_hash,
                 "message": message
             }
         )
 
-    async def send(self):
-        queue_ob = await self.queue.get()
-        message = queue_ob["message"]
-        recv_agent = self.agents[queue_ob["receiver"]]
-        await recv_agent.write(message)
+    async def _send(self):
+        while True:
+            message = await self._queue.get()
+            sender_hash = message["sender"]
+
+            # send message to every agent but itself
+            for agent_hash in self._agents:
+                agent = self._agents[agent_hash]
+                if agent_hash != sender_hash:
+                    await agent.write(message)
+    
+    async def _gather_runners(self):
+        _runners = [agent.run(self) for agent in self._agents.values()] + [self._send()]
+        await asyncio.gather(
+            *_runners
+        )
     
     async def run(self):
-        while True:
-            await self.send()
+        await self._gather_runners()
+        return
