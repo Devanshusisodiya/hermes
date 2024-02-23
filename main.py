de8f3a49e76c532a53a542754d047c19bbc517f9
Devanshusisodiya <devanshukumar45@gmail.com>
2024-02-23 20:54:45 +0530

    refactored main

--- main.py
+++ main.py
@@ -1,34 +1,26 @@
 from hermes.agent import Agent
 from hermes.hermes import multiplexer
 import asyncio
-import random
-
-
-async def recv_handler(message):
-    print("[recv] : {}".format(message))
-    return False
-
-async def send_handler(message):
-    print("[sent] : {}".format(message))
-    return False
-
 
 async def main():
 
     a1 = Agent("a1")
     a2 = Agent("a2")
+    a3 = Agent("a3")
 
     await multiplexer.register(a1)
     await multiplexer.register(a2)
+    await multiplexer.register(a3)
 
-    try:
-        await asyncio.gather(
-            a1.run(multiplexer, a2.name, "from a1"),
-            a2.run(multiplexer, a1.name, "from a2"),
-            multiplexer.run()
-        )
-    except KeyboardInterrupt:
-        pass
+    # trying to register already registered agent
+    await multiplexer.register(a3)
+
+    await asyncio.gather(
+        multiplexer.run()
+    )
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
